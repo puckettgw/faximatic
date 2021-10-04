5f1db4089ff0feba2fac0990ea08dcccd982be79
George <puckettgw@gmail.com>
2021-10-04 19:01:57 -0400

    ass

--- inbound_fax.py
+++ inbound_fax.py
@@ -29,59 +29,42 @@ def main():
         return R(status=200)
     if r.method == 'POST':
         app.logger.info("POST request")
+        fax_url = "http://nothing.net/?"+r.data
+        parsed = urlparse(fax_url)
+        qs = parse_qs(parsed.query)
+        
+        
+        media_url = (qs['MediaUrl'])
+        #print(media_url)
+        #print(qs)
+        print("Media URL: " + media_url[0])
+
+        mailgun_api_key = os.environ['MAILGUN_API_KEY']
+        sandbox = 'YOUR SANDBOX URL HERE'
+        #recipient = os.environ['FAX_RECIPIENT']
+
+
+
+        recipientfax = qs['To'][0]
+        print("Faxnumber " + recipientfax)
+        recipient=read_table_item('recipients', 'faxnumber', recipientfax)
+        
+        #print(recipient['Item']['emails'])
+
+
+
+        #request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(sandbox)
+        request_url = 'https://api.mailgun.net/v2/out.faximatic.com/messages'
+        request = requests.post(request_url, auth=('api', mailgun_api_key), data={
+            'from': 'fax@in.faximatic.com',
+            'to': recipient['Item']['emails'],
+            'subject': 'New fax from '+qs['From'][0],
+            'text': 'You have a new fax from '+qs['From'][0]+'. The PDF version of the fax is attached.'
+        },
+        files=[("attachment", ("fax.pdf", requests.get(media_url[0]).content))])
+
+        print('Status: {0}'.format(request.status_code))
+        print('Body:   {0}'.format(request.text))
         return R(status=200)
-    
-#    print("Event Passed to Handler: " + json.dumps(event))
-#    emailbody = json.loads(event['body'])
-#    print(emailbody.keys())
-#
-    
-#    print(event['body'])
-    
-    fax_url = "http://nothing.net/?"+event['body']
-    parsed = urlparse(fax_url)
-    qs = parse_qs(parsed.query)
-    
-    
-    media_url = (qs['MediaUrl'])
-    #print(media_url)
-    #print(qs)
-    print("Media URL: " + media_url[0])
-
-    mailgun_api_key = os.environ['MAILGUN_API_KEY']
-    sandbox = 'YOUR SANDBOX URL HERE'
-    #recipient = os.environ['FAX_RECIPIENT']
-
-
-
-    recipientfax = qs['To'][0]
-    print("Faxnumber " + recipientfax)
-    recipient=read_table_item('recipients', 'faxnumber', recipientfax)
-    
-    #print(recipient['Item']['emails'])
-
-
-
-    #request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(sandbox)
-    request_url = 'https://api.mailgun.net/v2/out.faximatic.com/messages'
-    request = requests.post(request_url, auth=('api', mailgun_api_key), data={
-        'from': 'fax@in.faximatic.com',
-        'to': recipient['Item']['emails'],
-        'subject': 'New fax from '+qs['From'][0],
-        'text': 'You have a new fax from '+qs['From'][0]+'. The PDF version of the fax is attached.'
-    },
-    files=[("attachment", ("fax.pdf", requests.get(media_url[0]).content))])
-
-    print('Status: {0}'.format(request.status_code))
-    print('Body:   {0}'.format(request.text))
-
-
-
-    response = {
-  "isBase64Encoded" : "false",
-  "statusCode": "200",
-  "headers": {  },
-  "body": json.dumps(event)
-}
-    return response
+ 
 
